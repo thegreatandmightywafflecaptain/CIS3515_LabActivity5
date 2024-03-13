6079409fb89c6fdf0d1486a9d4e5ce7dd0d99b54
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-03-13 15:33:44 -0400

    App no longer crashes when deleting the name list when it's empty.

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -37,8 +37,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<View>(R.id.deleteButton).setOnClickListener {
-            (names as MutableList).removeAt(spinner.selectedItemPosition)
-            (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+            if(spinner.getCount() != 0) {
+                (names as MutableList).removeAt(spinner.selectedItemPosition)
+                (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+            }
         }
 
     }
